babe3b79a4c78a5d0ea14b6821e00e9d527f0047
Farra <farhatchoudhary2@gmail.com>
2025-08-07 21:08:39 -0400

    New post and Edit profile modal

---
+++ pages/index.js
@@ -0,0 +1,40 @@
+function openProfileModal() { 
+  // change that to id in the future
+  document.querySelector('.modal__container').classList.add('modalisopened');
+  document.querySelector('.formLogin').classList.add('showformlogin');
+}
+function closeProfileModal() {
+  document.querySelector('.modal__container').classList.remove('modalisopened');
+  
+}
+const editProfileButton  = document.querySelector(".profile__edit-button")
+
+editProfileButton.addEventListener("click", openProfileModal)
+
+
+const modalCloseButton  = document.querySelector(".modal__close-button")
+
+modalCloseButton.addEventListener("click", closeProfileModal)
+
+
+function openPostModal() {
+  document.querySelector('#postModal').classList.add('modalisopened');
+  document.querySelector('#postModal .formLogin').classList.add('showformlogin');
+}
+
+function closePostModal() {
+  console.log("Heloo there")
+  document.querySelector('#postModal').classList.remove('modalisopened');
+}
+
+  const newPostButton  = document.querySelector(".profile__post-button")
+  console.log(newPostButton)
+
+newPostButton.addEventListener("click", openPostModal)
+
+
+
+const modalCloseBtn  = document.querySelector("#postModal .modal__close-btn")
+
+modalCloseBtn.addEventListener("click", closePostModal)
+
